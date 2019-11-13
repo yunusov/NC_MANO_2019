cdfbe498ce572054a3817b4d2f59c2d407f1abf6
yunusov <v300187@yandex.ru>
2019-11-13 15:03:03 +0400

    123

--- as_impl/src/main/java/ru/mano/aviasales/App.java
+++ as_impl/src/main/java/ru/mano/aviasales/App.java
@@ -1,4 +1,4 @@
-package ru.fulfilment1.ticketDealer;
+package ru.mano.aviasales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ as_impl/src/main/java/ru/mano/aviasales/controller/Index.java
@@ -0,0 +1,5 @@
+package ru.mano.aviasales.controller;
+
+public class Index {
+
+}
