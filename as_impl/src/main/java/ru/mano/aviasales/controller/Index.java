dcc1f6fd85b8dc1763a04b5b878b44123dd59204
yunusov <v300187@yandex.ru>
2019-11-13 15:04:38 +0400

    Update Index.java

--- as_impl/src/main/java/ru/mano/aviasales/controller/Index.java
+++ as_impl/src/main/java/ru/mano/aviasales/controller/Index.java
@@ -1,5 +1,14 @@
 package ru.mano.aviasales.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
 public class Index {
+  
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
 
 }
